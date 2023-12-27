10dfe949143d4fc5546eefb970222766cf848978
skamen13 <maximusgilgenberg@gmail.com>
2023-12-27 21:31:50 +0300

    new

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SETTINGS: Map<string, string> = new Map([
+    ["CONVAI-API-KEY", "9e9b4b6f3a9728777efe45752d4b28ac"],
+    ["CHARACTER-ID", "15366c64-6ead-11ee-bf04-42010a40000b"]
+]);;
